38d4a10c36d771871c56b4bc142d32d5eda60821
th0usandTW <75797332+th0usandTW@users.noreply.github.com>
2020-12-11 13:04:23 +0800

    Find the Duplicate Number

---
+++ 287_set.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        set<int> s;
+        
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (s.find(nums[i]) != s.end())
+            {
+                return nums[i];
+            }
+            else
+            {
+                s.insert(nums[i]);
+            }
+        }
+        
+        return 0;
+    }
+};
