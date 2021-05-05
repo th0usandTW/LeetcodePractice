83a97a2109d5d8ee9b2e31a718000bd18603c789
th0usandTW <75797332+th0usandTW@users.noreply.github.com>
2021-05-05 22:38:52 +0800

    Update 121.cpp

--- 121.cpp
+++ 121.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<int> minPrice;
-        int MaxProfit = 0, profit;
+        int minPrice;
+        int maxProfit = 0, profit;
         
-        minPrice.push_back(prices[0]);
+        minPrice = prices[0];
         for (int i = 1; i < prices.size(); i++)
         {
-            // Only calculate profit when it > 0
-            if (prices[i] > minPrice[i-1])
+            if (prices[i] > minPrice)
             {
-                profit = prices[i] - minPrice[i-1];
-                if (profit > MaxProfit)
-                    MaxProfit = profit;
+                profit = prices[i] - minPrice;
+                if (profit > maxProfit)
+                    maxProfit = profit;
                 
-                // Update min price
-                minPrice.push_back(minPrice[i-1]);
             }
             else
-                minPrice.push_back(prices[i]);  // Update min price
+                minPrice = prices[i];
         }
         
-        return MaxProfit;
+        return maxProfit;
     }
 };
