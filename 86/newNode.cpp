a8df0173462b4fdf51a907b56ffe089c2cfa1087
th0usandTW <75797332+th0usandTW@users.noreply.github.com>
2021-05-10 20:24:13 +0800

    Partition List

---
+++ 86/newNode.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* cur = head;
+        ListNode* dummy1 = new ListNode(0);
+        ListNode* dummy2 = new ListNode(0);
+        ListNode* smallList = dummy1;
+        ListNode* bigList = dummy2;
+        
+        while (cur != NULL)
+        {
+            if (cur->val < x)
+            {
+                smallList->next = new ListNode(cur->val);
+                smallList = smallList->next;
+            }
+            else
+            {
+                bigList->next = new ListNode(cur->val);
+                bigList = bigList->next;
+            }
+            
+            cur = cur->next;
+        }
+        
+        smallList->next = dummy2->next;
+        return dummy1->next;
+    }
+};
