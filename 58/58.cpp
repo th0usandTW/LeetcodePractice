6a8aaaf8d2563dfb2cd8b804a53a66602123fe9f
th0usandTW <75797332+th0usandTW@users.noreply.github.com>
2021-05-17 22:45:33 +0800

    Length of Last Word

---
+++ 58/58.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        bool found = false;
+        for (int i = s.size()-1; i >= 0; i--) {
+            if (s[i] != ' ') {
+                count++;
+                found = true;
+            }
+            if (found && s[i] == ' ')
+                break;
+        }
+        
+        return count;
+    }
+};
