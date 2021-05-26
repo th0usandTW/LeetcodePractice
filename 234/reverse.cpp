009ea27d167b9b8600973fe511cf322501dc5e1d
th0usandTW <75797332+th0usandTW@users.noreply.github.com>
2021-05-26 21:35:06 +0800

    Create reverse.cpp

---
+++ 234/reverse.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        
+        if (head == NULL || head->next == NULL)
+            return true;
+        
+        while (fast->next && fast->next->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        ListNode *rev = reverseList(slow->next);
+        slow->next = NULL;
+        
+        while (rev)
+        {
+            if (rev->val != head->val)
+                return false;
+            rev = rev->next;
+            head = head->next;
+        }
+        
+        return true;
+    }
+    
+    ListNode *reverseList(ListNode* head) {
+        ListNode *pre = NULL, *next = NULL;
+        
+        while (head)
+        {
+            next = head->next;
+            head->next = pre;
+            pre = head;
+            head = next;
+        }
+        
+        return pre;
+    }
+};
